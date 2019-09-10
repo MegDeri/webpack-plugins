a2ef431848eef4ad913129b56dc27a845406d66c
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-10 21:43:10 +0300

    Change app TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import style from './TodoList.css';
 
+class Li extends React.Component {
+    render (){
+        return (<li onClick={this.props.remove}>{this.props.text}</li>);
+    }
+}
+
+
 const TodoApp = props => {
     
     const listElement = props.list.map(elem => <li key={elem.id} id={elem.id} remove={props.removeTodo}>{elem.text}</li>)
     return (
         <ul className={style.todoApp}>
             <h4>{props.name}</h4>
-            <div onClick={() => this.props.removeTodo(this.props.id)}>
-                {listElement}
-            </div>
+            
+           <Li> {listElement} </Li>    
+            
         </ul> 
     )
 }
